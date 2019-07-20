f0ea5358b17500a45e8abac4eb6bd9112542468a
jet Li <923526125@qq.com>
2019-07-21 00:11:37 +0800

    Update UserLoadBalance.java

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -141,7 +141,7 @@ public class UserLoadBalance implements LoadBalance {
             }
         }
         if(selectedInvoker!=null){
-            System.out.println(selectedInvoker.getUrl().getPort());
+//             System.out.println(selectedInvoker.getUrl().getPort());
             return selectedInvoker;
         }
         // should not happen here
